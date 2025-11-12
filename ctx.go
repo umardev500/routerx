3bc6130001a0eb1a8cd14fc3ee4e959116af7a35
Umar <umar.dev500@gmail.com>
2025-11-12 09:46:19 +0700

    ♻️ Update docs

--- ctx.go
+++ ctx.go
@@ -23,23 +23,23 @@ func NewCtx(w http.ResponseWriter, r *http.Request, hs []Handler) *Ctx {
 	}
 }
 
-// BodyParser parse the json request body.
-// Make sure that body paramter is a pointer.
+// BodyParser parses the JSON request body.
+// Make sure the body parameter is a pointer.
 func (c *Ctx) BodyParser(body any) error {
 	return json.NewDecoder(c.Request.Body).Decode(body)
 }
 
-// Context return the request context.
+// Context returns the request context.
 func (c *Ctx) Context() context.Context {
 	return c.Request.Context()
 }
 
-// WithContext set the request context.
+// WithContext sets the request context.
 func (c *Ctx) WithContext(ctx context.Context) {
 	c.Request = c.Request.WithContext(ctx)
 }
 
-// Next call the next handler
+// Next calls the next handler.
 func (c *Ctx) Next() error {
 	if c.index >= len(c.handlers) {
 		return nil
@@ -51,19 +51,19 @@ func (c *Ctx) Next() error {
 	return handler(c)
 }
 
-// Status set response status code
+// Status sets the response status code.
 func (c *Ctx) Status(code int) *Ctx {
 	c.status = code
 	return c
 }
 
-// SendStatus send response just only status code
+// SendStatus sends a response with only a status code.
 func (c *Ctx) SendStatus(code int) error {
 	c.Writer.WriteHeader(code)
 	return nil
 }
 
-// Json send json response
+// Json sends a JSON response.
 func (c *Ctx) Json(data any) error {
 	code := http.StatusOK
 	if c.status != 0 {
@@ -74,8 +74,8 @@ func (c *Ctx) Json(data any) error {
 	return json.NewEncoder(c.Writer).Encode(data)
 }
 
-// Query get the query parameter value by the given key.
-// If there no values associated with the key, Query returns the default value.
+// Query returns the query paramter value for the given key.
+// If there are not values associated with the key, Query returns the default value.
 func (c *Ctx) Query(key string, defaultVal ...string) string {
 	val := c.Request.URL.Query().Get(key)
 
@@ -86,9 +86,9 @@ func (c *Ctx) Query(key string, defaultVal ...string) string {
 	return val
 }
 
-// QueryInt returns the query paramter value int by the given key.
-// If there no values associated with the key, QueryInt returns the default value.
-// If the default falue is not provided then it returns the zero value of int.
+// QueryInt returns the query paramter value as int for the given key.
+// If there are no values associated with the key, QueryInt returns the default value.
+// If the default value is not provided, it returns the zero value of int.
 func (c *Ctx) QueryInt(key string, defaultVal ...int) int {
 	defaultValue := "0"
 	if len(defaultVal) > 0 {
@@ -102,9 +102,9 @@ func (c *Ctx) QueryInt(key string, defaultVal ...int) int {
 	return val
 }
 
-// QueryBool returns the query parameter value bool by the given key.
-// If there no values associated with the key, QueryBool returns the default value.
-// If the default value is not provided then it returns the zero value of bool.
+// QueryBool returns the query paramter value as bool for the given key.
+// If there are no values associated with the key, QueryBool returns the default value.
+// If the default value is not provided, it returns the zero value of bool.
 func (c *Ctx) QueryBool(key string, defaulVal ...bool) bool {
 	defaultValue := "false"
 	if len(defaulVal) > 0 {
